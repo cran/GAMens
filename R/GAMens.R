5934bc5277535d1c7901acee7576e21314940dc3
Koen W. De Bock <kdebock@audencia.com>
2018-04-05 16:12:34 +0000

    version 1.2.1

--- R/GAMens.R
+++ R/GAMens.R
@@ -1,5 +1,137 @@
-GAMens <- 
-function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE, rsm=TRUE, fusion="avgagg") 
+#' Applies the GAMbag, GAMrsm or GAMens ensemble classifier to a data set
+#'
+#' Fits the GAMbag, GAMrsm or GAMens ensemble algorithms for binary
+#' classification using generalized additive models as base classifiers.
+#'
+#' The \code{GAMens} function applies the GAMbag, GAMrsm or GAMens ensemble
+#' classifiers (De Bock et al., 2010) to a data set. GAMens is the default with
+#' (\code{bagging=TRUE} and \code{rsm=TRUE}. For GAMbag, \code{rsm} should be
+#' specified as \code{FALSE}.  For GAMrsm, \code{bagging} should be
+#' \code{FALSE}.
+#'
+#' The \code{GAMens} function provides the possibility for automatic formula
+#' specification. In this case, dichotomous variables in \code{data} are
+#' included as linear terms, and other variables are assumed continuous,
+#' included as nonparametric terms, and estimated by means of smoothing
+#' splines. To enable automatic formula specification, use the generic formula
+#' \code{[response variable name]~.} in combination with \code{autoform =
+#' TRUE}. Note that in this case, all variables available in \code{data} are
+#' used in the model. If a formula other than \code{[response variable name]~.}
+#' is specified then the \code{autoform} option is automatically overridden. If
+#' \code{autoform=FALSE} and the generic formula \code{[response variable
+#' name]~.} is specified then the GAMs in the ensemble will not contain
+#' nonparametric terms (i.e., will only consist of linear terms).
+#'
+#' Four alternative fusion rules for member classifier outputs can be
+#' specified. Possible values are \code{'avgagg'} for average aggregation
+#' (default), \code{'majvote'} for majority voting, \code{'w.avgagg'} for
+#' weighted average aggregation, or \code{'w.majvote'} for weighted majority
+#' voting.  Weighted approaches are based on member classifier error rates.
+#'
+#' @param formula a formula, as in the \code{gam} function. Smoothing splines
+#' are supported as nonparametric smoothing terms, and should be indicated by
+#' \code{s}. See the documentation of \code{s} in the \code{gam} package for
+#' its arguments. The \code{GAMens} function also provides the possibility for
+#' automatic \code{formula} specification. See 'details' for more information.
+#' @param data a data frame in which to interpret the variables named in
+#' \code{formula}.
+#' @param rsm_size an integer, the number of variables to use for random
+#' feature subsets used in the Random Subspace Method. Default is 2.  If
+#' \code{rsm=FALSE}, the value of \code{rsm_size} is ignored.
+#' @param autoform if \code{FALSE} (default), the model specification in
+#' \code{formula} is used. If \code{TRUE}, the function triggers automatic
+#' \code{formula} specification. See 'details' for more information.
+#' @param iter an integer, the number of base classifiers (GAMs) in the
+#' ensemble. Defaults to \code{iter=10} base classifiers.
+#' @param df an integer, the number of degrees of freedom (df) used for
+#' smoothing spline estimation. Its value is only used when \code{autoform =
+#' TRUE}. Defaults to \code{df=4}. Its value is ignored if a formula is
+#' specified and \code{autoform} is \code{FALSE}.
+#' @param bagging enables Bagging if value is \code{TRUE} (default). If
+#' \code{FALSE}, Bagging is disabled. Either \code{bagging}, \code{rsm} or both
+#' should be \code{TRUE}
+#' @param rsm enables Random Subspace Method (RSM) if value is \code{TRUE}
+#' (default). If \code{FALSE}, RSM is disabled. Either \code{bagging},
+#' \code{rsm} or both should be \code{TRUE}
+#' @param fusion specifies the fusion rule for the aggregation of member
+#' classifier outputs in the ensemble. Possible values are \code{'avgagg'}
+#' (default), \code{'majvote'}, \code{'w.avgagg'} or \code{'w.majvote'}.
+#' @return An object of class \code{GAMens}, which is a list with the following
+#' components: \item{GAMs}{the member GAMs in the ensemble.} \item{formula}{the
+#' formula used tot create the \code{GAMens} object.  } \item{iter}{the
+#' ensemble size. } \item{df}{number of degrees of freedom (df) used for
+#' smoothing spline estimation. } \item{rsm}{indicates whether the Random
+#' Subspace Method was used to create the \code{GAMens} object. }
+#' \item{bagging}{indicates whether bagging was used to create the
+#' \code{GAMens} object. } \item{rsm_size}{the number of variables used for
+#' random feature subsets. } \item{fusion_method}{the fusion rule that was used
+#' to combine member classifier outputs in the ensemble. } \item{probs}{the
+#' class membership probabilities, predicted by the ensemble classifier.  }
+#' \item{class}{the class predicted by the ensemble classifier. }
+#' \item{samples}{an array indicating, for every base classifier in the
+#' ensemble, which observations were used for training. } \item{weights}{a
+#' vector with weights defined as (1 - error rate). Usage depends upon
+#' specification of \code{fusion_method}. }
+#' @export
+#' @author Koen W. De Bock \email{kdebock@@audencia.com}, Kristof Coussement
+#' \email{K.Coussement@@ieseg.fr} and Dirk Van den Poel
+#' \email{Dirk.VandenPoel@@ugent.be}
+#' @seealso \code{\link{predict.GAMens}}, \code{\link{GAMens.cv}}
+#' @references De Bock, K.W. and Van den Poel, D. (2012):
+#' "Reconciling Performance and Interpretability in Customer Churn Prediction Modeling
+#' Using Ensemble Learning Based on Generalized Additive Models".
+#' Expert Systems With Applications, Vol 39, 8, pp. 6816--6826.
+#'
+#' De Bock, K. W., Coussement, K. and Van den Poel, D. (2010):
+#' "Ensemble Classification based on generalized additive models".
+#' Computational Statistics & Data Analysis, Vol 54, 6, pp. 1535--1546.
+#'
+#' Breiman, L. (1996): "Bagging predictors". Machine Learning, Vol 24, 2, pp.
+#' 123--140.
+#'
+#' Hastie, T. and Tibshirani, R. (1990): "Generalized Additive Models", Chapman
+#' and Hall, London.
+#'
+#' Ho, T. K. (1998): "The random subspace method for constructing decision
+#' forests". IEEE Transactions on Pattern Analysis and Machine Intelligence,
+#' Vol 20, 8, pp. 832--844.
+#' @keywords models classif
+#' @import mlbench caTools mlbench
+#' @importFrom stats as.formula binomial predict terms
+#' @examples
+#'
+#'
+#' ## Load data (mlbench library should be loaded)
+#' library(mlbench)
+#' data(Ionosphere)
+#' IonosphereSub<-Ionosphere[,c("V1","V2","V3","V4","V5","Class")]
+#'
+#' ## Train GAMens using all variables in Ionosphere dataset
+#' Ionosphere.GAMens <- GAMens(Class~., IonosphereSub ,4 , autoform=TRUE,
+#' iter=10 )
+#'
+#' ## Compare classification performance of GAMens, GAMrsm and GAMbag ensembles,
+#' ## using 4 nonparametric terms and 2 linear terms
+#' Ionosphere.GAMens <- GAMens(Class~s(V3,4)+s(V4,4)+s(V5,3)+s(V6,5)+V7+V8,
+#' Ionosphere ,3 , autoform=FALSE, iter=10 )
+#'
+#' Ionosphere.GAMrsm <- GAMens(Class~s(V3,4)+s(V4,4)+s(V5,3)+s(V6,5)+V7+V8,
+#' Ionosphere ,3 , autoform=FALSE, iter=10, bagging=FALSE, rsm=TRUE )
+#'
+#' Ionosphere.GAMbag <- GAMens(Class~s(V3,4)+s(V4,4)+s(V5,3)+s(V6,5)+V7+V8,
+#' Ionosphere ,3 , autoform=FALSE, iter=10, bagging=TRUE, rsm=FALSE )
+#'
+#' ## Calculate AUCs (for function colAUC, load caTools library)
+#' library(caTools)
+#' GAMens.auc <- colAUC(Ionosphere.GAMens[[9]], Ionosphere["Class"]=="good",
+#' plotROC=FALSE)
+#' GAMrsm.auc <- colAUC(Ionosphere.GAMrsm[[9]], Ionosphere["Class"]=="good",
+#' plotROC=FALSE)
+#' GAMbag.auc <- colAUC(Ionosphere.GAMbag[[9]], Ionosphere["Class"]=="good",
+#' plotROC=FALSE)
+#'
+GAMens <-
+function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE, rsm=TRUE, fusion="avgagg")
 {
 
 	formula<- as.formula(formula)
@@ -25,7 +157,7 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 		stop("rsm_size should be smaller than the number of explanatory variables specified in formula")
 	}
 
-	targets <- data[,as.character(formula[[2]])]  
+	targets <- data[,as.character(formula[[2]])]
 	nclasses <- nlevels(targets)
 	depvarname <- as.character(formula[[2]])
 
@@ -35,7 +167,7 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 	newdata = cbind(data[,1:ncol(data)-1],newtargets)
 	names(newdata)[ncol(newdata)] <- depvarname
 
-	
+
 	n <- length(data[,1])
 	if (formula[3] == ".()"){
 		p <- (length(newdata[1,])-1)
@@ -44,19 +176,19 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 		varnames <- as.matrix(setdiff.data.frame(as.matrix(cbind(all.vars(formula))),as.matrix(depvarname)))
 		formula_terms <- attr(terms(formula),"term.labels")
 	}
-	
-     	gam_models <- list() 
+
+     	gam_models <- list()
 	oob_predictions_p <- data.frame(cbind(1:nrow(newdata)))
 	oob_predictions_c <- data.frame(cbind(1:nrow(newdata)))
 	names(oob_predictions_p) <- "ID"
 	names(oob_predictions_c) <- "ID"
 	bootstraps <- array(0, c(n,iter))
 	errors <- array(0,iter)
-	
+
 	id_added <- cbind(newdata,1:nrow(newdata))
 	names(id_added)[ncol(id_added)] <- "ID"
 	cutoff <- 0.5
-	treshold <- 2	
+	treshold <- 2
 	for (m in 1:iter) {
 		flag <- FALSE
 		gam_model <- NA
@@ -71,7 +203,7 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 					for (vn in 1:length(selectedvars)) {
 						varname <- selectedvars[vn]
 						uniq <- dim(unique(as.data.frame(data[bootstrap, rfs[vn]])))[1]
-						if (uniq<=treshold) {rfs_linvars <- rbind(rfs_linvars,varname) } 
+						if (uniq<=treshold) {rfs_linvars <- rbind(rfs_linvars,varname) }
 						if (uniq>treshold) {rfs_nparvars <- rbind(rfs_nparvars,varname) }
 					}
 					if (length(rfs_nparvars > 0)) {
@@ -80,7 +212,7 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 					} else {npar_form2 <- character(0)}
 					if (length(rfs_linvars > 0)) {
 						lin_form <- paste(rfs_linvars,collapse= "+")
-						if (length(rfs_nparvars > 0)) {finalstring = paste(depvarname,"~",npar_form2,"+",lin_form)} 
+						if (length(rfs_nparvars > 0)) {finalstring = paste(depvarname,"~",npar_form2,"+",lin_form)}
 						else {finalstring <- paste(depvarname,"~",lin_form)}
 					} else {finalstring = paste(depvarname,"~",npar_form2)}
 					fmla <- as.formula(finalstring)
@@ -97,11 +229,11 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 				fmla <- as.formula(finalstring)
 			}
 
-		try(gam_model <- gam(fmla,data=newdata[bootstrap,], family=binomial(link="logit")),silent=TRUE)	
+		try(gam_model <- gam(fmla,data=newdata[bootstrap,], family=binomial(link="logit")),silent=TRUE)
 		}
 		if (bagging == TRUE) {oob_data <- setdiff.data.frame(id_added,id_added[bootstrap,])} else {oob_data <- id_added[bootstrap,]}
 		oob_cnt <- nrow(oob_data)
-		oob_predict <- predict.gam(gam_model,oob_data,type="response")
+		oob_predict <- predict(gam_model,oob_data,type="response")
 		oob_predict_r <- as.data.frame(cbind(as.numeric(oob_predict),oob_data[,"ID"]))
 		names(oob_predict_r)[2] <- "ID"
 		coln <- paste("pred",m,sep="")
@@ -109,13 +241,13 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 		oob_predict_c <- as.data.frame(cbind((oob_predict_r[,1] > cutoff),oob_predict_r[,2]))
 		names(oob_predict_c)[2] <- "ID"
 		names(oob_predict_c)[1] <- coln
-		ind<-as.numeric(oob_data[depvarname] != oob_predict_c[,1]) 	
+		ind<-as.numeric(oob_data[depvarname] != oob_predict_c[,1])
 		err<- sum(ind)/oob_cnt
 		errors[m] <- err
-		                     
+
 		oob_predictions_p <- merge(oob_predictions_p,oob_predict_r, by = "ID", all= "TRUE")
 		oob_predictions_c <- merge(oob_predictions_c,oob_predict_c, by = "ID", all= "TRUE")
-		gam_models[[m]] <- gam_model			
+		gam_models[[m]] <- gam_model
 		bootstraps[,m]<-bootstrap
 
 	}
@@ -126,7 +258,7 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 	temp_sums <- rowSums(temp_oob_pred_p[,2:ncol(temp_oob_pred_p)])
 	temp_sums_cl <- rowSums(temp_oob_pred_c[,2:ncol(temp_oob_pred_p)])
 	temp_n <- rowSums(!is.na(oob_predictions_p[,2:ncol(temp_oob_pred_p)]))
-	
+
 	if (fusion == "avgagg") {
 		pred <- cbind(temp_sums / temp_n)
 		class <- as.numeric(pred > cutoff)
@@ -145,8 +277,8 @@ function(formula, data, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE,
 		class <- as.numeric(pred > cutoff)}
 	class <-  cbind(gsub(1,target_classes_s[[2]],class,fixed=FALSE))
 	class <-  cbind(gsub(0,target_classes_s[[1]],class,fixed=FALSE))
-	
+
 	ans<- list(GAMs=gam_models, formula=fmla, iter=iter, df=df, rsm=rsm, bagging=bagging, rsm_size=rsm_size, fusion_method=fusion, probs=pred, class=class, samples=bootstraps, weights=1-errors)
 	class(ans) <- "GAMens"
-	ans	
+	ans
 }
--- R/GAMens.cv.R
+++ R/GAMens.cv.R
@@ -1,10 +1,104 @@
-GAMens.cv <- 
-function (formula, data, cv, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE, rsm=TRUE, fusion="avgagg") 
+#' Runs v-fold cross validation with GAMbag, GAMrsm or GAMens ensemble
+#' classifier
+#'
+#' In v-fold cross validation, the data are divided into \code{v} subsets of
+#' approximately equal size. Subsequently, one of the \code{v} data parts is
+#' excluded while the remainder of the data is used to create a \code{GAMens}
+#' object.  Predictions are generated for the excluded data part. The process
+#' is repeated \code{v} times.
+#'
+#'
+#' @param formula a formula, as in the \code{gam} function. Smoothing splines
+#' are supported as nonparametric smoothing terms, and should be indicated by
+#' \code{s}. See the documentation of \code{s} in the \code{gam} package for
+#' its arguments. The \code{GAMens} function also provides the possibility for
+#' automatic \code{formula} specification. See 'details' for more information.
+#' @param data a data frame in which to interpret the variables named in
+#' \code{formula}.
+#' @param cv An integer specifying the number of folds in the cross-validation.
+#' @param rsm_size an integer, the number of variables to use for random
+#' feature subsets used in the Random Subspace Method. Default is 2.  If
+#' \code{rsm=FALSE}, the value of \code{rsm_size} is ignored.
+#' @param autoform if \code{FALSE} (by default), the model specification in
+#' \code{formula} is used. If \code{TRUE}, the function triggers automatic
+#' \code{formula} specification. See 'details' for more information.
+#' @param iter an integer, the number of base (member) classifiers (GAMs) in
+#' the ensemble. Defaults to \code{iter=10} base classifiers.
+#' @param df an integer, the number of degrees of freedom (df) used for
+#' smoothing spline estimation. Its value is only used when \code{autoform =
+#' TRUE}. Defaults to \code{df=4}. Its value is ignored if a formula is
+#' specified and \code{autoform} is \code{FALSE}.
+#' @param bagging enables Bagging if value is \code{TRUE} (default). If
+#' \code{FALSE}, Bagging is disabled. Either \code{bagging}, \code{rsm} or both
+#' should be \code{TRUE}
+#' @param rsm enables Random Subspace Method (RSM) if value is \code{TRUE}
+#' (default). If \code{FALSE}, rsm is disabled. Either \code{bagging},
+#' \code{rsm} or both should be \code{TRUE}
+#' @param fusion specifies the fusion rule for the aggregation of member
+#' classifier outputs in the ensemble. Possible values are \code{'avgagg'} for
+#' average aggregation (default), \code{'majvote'} for majority voting,
+#' \code{'w.avgagg'} for weighted average aggregation based on base classifier
+#' error rates, or \code{'w.majvote'} for weighted majority voting.
+#' @return An object of class \code{GAMens.cv}, which is a list with the
+#' following components: \item{foldpred}{a data frame with, per fold, predicted
+#' class membership probabilities for the left-out observations. }
+#' \item{pred}{a data frame with predicted class membership probabilities. }
+#' \item{foldclass}{a data frame with, per fold, predicted classes for the
+#' left-out observations. } \item{class}{a data frame with predicted classes. }
+#' \item{conf}{the confusion matrix which compares the real versus predicted
+#' class memberships, based on the \code{class} object. }
+#' @author Koen W. De Bock \email{kdebock@@audencia.com}, Kristof Coussement
+#' \email{K.Coussement@@ieseg.fr} and Dirk Van den Poel
+#' \email{Dirk.VandenPoel@@ugent.be}
+#' @seealso \code{\link{predict.GAMens}}, \code{\link{GAMens}}
+#' @references De Bock, K.W. and Van den Poel, D. (2012):
+#' "Reconciling Performance and Interpretability in Customer Churn Prediction Modeling
+#' Using Ensemble Learning Based on Generalized Additive Models".
+#' Expert Systems With Applications, Vol 39, 8, pp. 6816--6826.
+#'
+#' De Bock, K. W., Coussement, K. and Van den Poel, D. (2010):
+#' "Ensemble Classification based on generalized additive models".
+#' Computational Statistics & Data Analysis, Vol 54, 6, pp. 1535--1546.
+#'
+#' Breiman, L. (1996): "Bagging predictors". Machine Learning, Vol 24, 2, pp.
+#' 123--140.
+#'
+#' Hastie, T. and Tibshirani, R. (1990): "Generalized Additive Models", Chapman
+#' and Hall, London.
+#'
+#' Ho, T. K. (1998): "The random subspace method for constructing decision
+#' forests". IEEE Transactions on Pattern Analysis and Machine Intelligence,
+#' Vol 20, 8, pp. 832--844.
+#' @keywords models classif
+#' @export
+#' @examples
+#'
+#' ## Load data: mlbench library should be loaded!)
+#' library(mlbench)
+#' data(Sonar)
+#' SonarSub<-Sonar[,c("V1","V2","V3","V4","V5","V6","Class")]
+#'
+#' ## Obtain cross-validated classification performance of GAMrsm
+#' ## ensembles, using all variables in the Sonar dataset, based on 5-fold
+#' ## cross validation runs
+#'
+#' Sonar.cv.GAMrsm <- GAMens.cv(Class~s(V1,4)+s(V2,3)+s(V3,4)+V4+V5+V6,
+#' SonarSub ,5, 4 , autoform=FALSE, iter=10, bagging=FALSE, rsm=TRUE )
+#'
+#' ## Calculate AUCs (for function colAUC, load caTools library)
+#' library(caTools)
+#'
+#' GAMrsm.cv.auc <- colAUC(Sonar.cv.GAMrsm[[2]], SonarSub["Class"]=="R",
+#' plotROC=FALSE)
+#'
+#'
+GAMens.cv <-
+function (formula, data, cv, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=TRUE, rsm=TRUE, fusion="avgagg")
 {
 	setdiff.data.frame <- function(A,B) A[ !duplicated( rbind(B,A) )[ -seq_len(nrow(B))] , ]
 	n <- dim(data)[1]
 	if(cv <2 | cv>n) {stop("The number of cross-validations should be larger than 2 and smaller than the number of observations")}
-	
+
 	bootstrap <- sample(1:n,replace=FALSE)
 	obs_per_fold <- floor(n/cv)
 	pred_per_fold <- as.data.frame(array(NA, c(n,cv)))
@@ -19,7 +113,7 @@ function (formula, data, cv, rsm_size=2, autoform=FALSE, iter=10, df=4, bagging=
 		} else {fold_ids <- bootstrap[(((c-1)*obs_per_fold):nrow(data))]}
 		traindata_folds <- setdiff.data.frame(data,fold)
 		GAMens_object <- GAMens(formula,traindata_folds, rsm_size, autoform=autoform,iter=iter,df=df,bagging=bagging,rsm=rsm,fusion=fusion)
-		results <- predict.GAMens(GAMens_object,fold)
+		results <- predict(GAMens_object,fold)
 		pred_per_fold[fold_ids,c] <- results[[1]]
 		names(pred_per_fold)[[c]] <- paste("fold",c,sep="")
 		pred[fold_ids,1] <- results[[1]]
